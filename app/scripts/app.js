e4cee21aa497eeed621c12985872f4dd8d363475
Hema Sai Durga Dharanipragada <hdharanipragada@innominds.com>
2017-03-23 12:57:26 +0530

    removed compass dependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,18 +11,25 @@
 angular
   .module('ybhApp', [
     'ngAnimate',
+    'ngAria',
     'ngCookies',
+    'ngMessages',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider,$locationProvider) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on 2017-03-22
+// Generated on 2017-03-23
 
 module.exports = function(config) {
   'use strict';
@@ -22,9 +22,11 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
